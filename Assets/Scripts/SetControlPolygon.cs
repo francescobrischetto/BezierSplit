0fe9f734cf9e79943de75b09564cf97909e5addd
Francesco Brischetto <francesco_brischetto@hotmail.it>
2021-01-02 22:28:24 +0100

    Aggiunta logica del taglio della superficie. Rimossi Asset e scene inutili. Sistemato calcolo volume

---
+++ Assets/Scripts/SetControlPolygon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetControlPolygon : MonoBehaviour
+{
+    public GameObject BezierSurface;
+    private MeshGenerator BezierSurfaceScript;
+    public bool visible = false;
+    Mesh m;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BezierSurfaceScript = BezierSurface.GetComponent<MeshGenerator>();
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSecondsRealtime(0.01f);
+        m = new Mesh();
+        if (visible) GetComponent<MeshFilter>().mesh = m;
+        //aggiorna la mesh
+        m.Clear();
+        m.vertices = BezierSurfaceScript.linearControlPoints;
+        m.triangles = BezierSurfaceScript.controlTriangles;
+        m.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (visible) GetComponent<MeshFilter>().mesh = m;
+        else GetComponent<MeshFilter>().mesh = new Mesh();
+    }
+}
