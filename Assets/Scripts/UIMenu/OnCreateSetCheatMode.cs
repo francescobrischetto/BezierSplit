80bf1dc2736b254efa366fc729dfd7e8e092ba7a
Marco <mghezzi-dev@users.noreply.github.com>
2021-01-16 11:30:24 +0100

    Cheat mode fix

---
+++ Assets/Scripts/UIMenu/OnCreateSetCheatMode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnCreateSetCheatMode : MonoBehaviour
+{
+    public Toggle _toggle;
+
+    void Start()
+    {
+        FindObjectOfType<CheatMode>()._cheatMode = _toggle.isOn;
+    }
+}
